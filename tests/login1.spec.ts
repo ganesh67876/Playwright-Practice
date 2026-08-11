6db7e3322f4306d01672e93b0a784307fe8feafe
saiganeshg-sys <saiganesh.g@terralogic.com>
2026-08-11 09:00:11 +0530

    Update Playwright login test

--- tests/login1.spec.ts
+++ tests/login1.spec.ts
@@ -3,5 +3,5 @@ import { test, expect } from '@playwright/test';
 test('CI Login Demo', async ({ page }) => {
     await page.goto('https://stgclickscan.terralogic.com/');
     
-    await expect(page.locator("p[class$='styles_signInSubtitle__qCq6i']")).toHaveText('Access your AI document management portal');
+    await expect(page).toHaveTitle("ClickScan");
 });
