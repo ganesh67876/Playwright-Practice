b1c215e6ab2e403ffd7ad06f8ebeaccb0ed1daab
saiganeshg-sys <saiganesh.g@terralogic.com>
2026-08-11 09:15:33 +0530

    updated playwright login test

--- tests/login1.spec.ts
+++ tests/login1.spec.ts
@@ -3,5 +3,6 @@ import { test, expect } from '@playwright/test';
 test('CI Login Demo', async ({ page }) => {
     await page.goto('https://stgclickscan.terralogic.com/');
     
-    await expect(page).toHaveTitle("ClickScan");
+    const usernameInput = page.getByText("Username")
+    await expect(usernameInput).toBeVisible();
 });
